4a9aa7332c7d69a91f37aa22293d51685e9054d5
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-02-10 14:55:17 +0200

    FEAT: Add abstract character class, add primary attribute class, add mage sub class

---
+++ Attributes/PrimaryAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Attributes
+{
+    internal class PrimaryAttribute
+    {
+        private int Strength { get; set; }
+        private int Dexterity { get; set; }
+        private int Intelligence { get; set; }
+
+        public PrimaryAttribute(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+
+        public static PrimaryAttribute operator +(PrimaryAttribute lhs, PrimaryAttribute rhs)
+        {
+            return new PrimaryAttribute(
+                lhs.Strength + rhs.Strength, 
+                lhs.Dexterity + rhs.Dexterity,
+                lhs.Intelligence + rhs.Intelligence
+            );
+        }
+
+        public void incrementStrengthBy(int amount)
+        {
+            Strength += amount;
+        }
+
+        public void incrementDexterityBy(int amount)
+        {
+            Dexterity += amount;
+        }
+
+        public void incrementIntelligenceBy(int amount)
+        {
+            Intelligence += amount;
+        }
+    }
+}
---
+++ Characters/Character.cs
@@ -0,0 +1,32 @@
+﻿using RPGCharacters.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Characters
+{
+    public enum characterClass
+    {
+        Mage,
+        Ranger,
+        Rogue,
+        Warrior
+    }
+    abstract class Character
+    {
+        protected string Name { get; set; }
+        protected int Level { get; set; }
+        protected characterClass CharacterClass { get; set; }
+        protected PrimaryAttribute BasePrimaryAttributes;
+
+        public Character()
+        {
+            Level = 1;
+        }
+
+        public abstract void levelUp();
+
+    }
+}
