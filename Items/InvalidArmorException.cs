faab576cc77c992cf9d36200cf5840b2245eab8d
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-02-11 11:38:05 +0200

    FEAT: Add custom exceptions for weapons and armor

---
+++ Items/InvalidArmorException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Items
+{
+    internal class InvalidArmorException : Exception
+    {
+        public InvalidArmorException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message => "The piece of armor cannot be equipped.";
+    }
+}
---
+++ Items/InvalidWeaponException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacters.Items
+{
+    internal class InvalidWeaponException : Exception
+    {
+        public InvalidWeaponException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message => "The weapon cannot be equipped.";
+    }
+}
